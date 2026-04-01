f64c0a475af76e2a5ebbaddf23029ed5f0080f03
范文斌 <1276782778@qq.com>
2026-04-01 20:58:08 +0800

    Add test placeholder in DepartServiceImpl

--- src/main/java/com/ch/personmis/service/DepartServiceImpl.java
+++ src/main/java/com/ch/personmis/service/DepartServiceImpl.java
@@ -44,6 +44,7 @@ public class DepartServiceImpl implements DepartService {
         map.put("departs", departs);
         departByCon.setAct("byNoPage");
         map.put("total", departRepository.selectDepartmentsByCon(departByCon).size());
+        @@测试用@@
         return map;
     }
 
