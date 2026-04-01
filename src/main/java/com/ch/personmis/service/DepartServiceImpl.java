ebf52ee096922f0b3a86e289f2e09424a3d3239e
CarrotIceBin <1276782778@qq.com>
2026-04-01 16:54:33 +0800

    删除测试

--- src/main/java/com/ch/personmis/service/DepartServiceImpl.java
+++ src/main/java/com/ch/personmis/service/DepartServiceImpl.java
@@ -44,6 +44,7 @@ public class DepartServiceImpl implements DepartService {
         map.put("departs", departs);
         departByCon.setAct("byNoPage");
         map.put("total", departRepository.selectDepartmentsByCon(departByCon).size());
+        System.out.print("12341");
         return map;
     }
 
