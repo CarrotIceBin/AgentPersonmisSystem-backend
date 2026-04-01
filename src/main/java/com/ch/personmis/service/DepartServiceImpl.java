3b175c11bd518a55e7e2093e8e3e5522e0e7761e
CarrotIceBin <1276782778@qq.com>
2026-04-01 17:06:57 +0800

    delete

--- src/main/java/com/ch/personmis/service/DepartServiceImpl.java
+++ src/main/java/com/ch/personmis/service/DepartServiceImpl.java
@@ -54,7 +54,6 @@ public class DepartServiceImpl implements DepartService {
         map.put("departs", departs);
         Depart aDepart = departRepository.getDepartmentDetail(id);
         map.put("aDepart", aDepart);
-        System.out.print("14124");
         return map;
     }
 
